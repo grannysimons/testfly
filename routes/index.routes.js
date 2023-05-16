b130b480fae3b1ee09312cb876b616aae551317c
Mariona Roca <marionaroca@MacBook-MacBook-Pro-de-Mariona.local>
2023-05-16 20:16:20 +0200

    User

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User.model');
+
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  User.findOne()
+  .then(resp => {
+    res.render("index");
+  })
+  .catch(err =>next(err))
 });
 
 module.exports = router;
